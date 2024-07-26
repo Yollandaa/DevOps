2139899a728e0b6a5192152414dc523de1c8405c
Caleb Potts <Caleb.Potts@sanlam.co.za>
2024-07-26 12:01:04 +0200

    updated the fetch_users function and added update and delete

--- routes/users_bp.py
+++ routes/users_bp.py
@@ -19,16 +19,17 @@ def fetch_users():
     with open(USERS_FILE, "r") as file:
         local_users = json.load(file)
 
-    users = local_users + api_users
-    return users
+    combined_users = local_users + [
+        user for user in api_users if user not in local_users
+    ]
+
+    with open(USERS_FILE, "w") as file:
+        json.dump(combined_users, file)
+
+    return combined_users
 
 
-def save_user(user):
-    with open(USERS_FILE, "r") as file:
-        users = json.load(file)
-
-    users.append(user)
-
+def save_users(users):
     with open(USERS_FILE, "w") as file:
         json.dump(users, file)
 
@@ -58,7 +59,53 @@ def get_user_by_name(name):
 def add_user():
     try:
         user_data = request.get_json()
-        save_user(user_data)
+        users = fetch_users()
+        users.append(user_data)
+        save_users(users)
         return jsonify({"message": "User added successfully"}), 201
+    except Exception as e:
+        return jsonify({"message": "An error occurred", "error": str(e)}), 500
+
+
+@user_bp.route("/<string:name>", methods=["PUT"])
+def update_user(name):
+    try:
+        user_data = request.get_json()
+        users = fetch_users()
+        user_updated = False
+        for user in users:
+            if (
+                user["name"]["firstname"].lower() == name.lower()
+                or user["name"]["lastname"].lower() == name.lower()
+            ):
+                user.update(user_data)
+                user_updated = True
+                break
+
+        if user_updated:
+            save_users(users)
+            return jsonify({"message": "User updated successfully"}), 200
+        else:
+            return jsonify({"message": "User not found"}), 404
+    except Exception as e:
+        return jsonify({"message": "An error occurred", "error": str(e)}), 500
+
+
+@user_bp.route("/<string:name>", methods=["DELETE"])
+def delete_user(name):
+    try:
+        users = fetch_users()
+        filtered_users = [
+            user
+            for user in users
+            if user["name"]["firstname"].lower() != name.lower()
+            and user["name"]["lastname"].lower() != name.lower()
+        ]
+
+        if len(filtered_users) == len(users):
+            return jsonify({"message": "User not found"}), 404
+
+        save_users(filtered_users)
+        return jsonify({"message": "User deleted successfully"}), 200
     except Exception as e:
         return jsonify({"message": "An error occurred", "error": str(e)}), 500
