a3bcd8004b835b331d0a829313b9c352fe9b97b1
Yollandaa <yolanda.dastile@sanlam.co.za>
2024-08-01 12:18:45 +0200

    Nikhil's changes

--- src/scripts/data/example_function.py
+++ src/scripts/data/example_function.py
@@ -38,3 +38,17 @@ def get_data(cart: bool = False, products: bool = False, users: bool = False):
 get_data(products=True)
 get_data(users=True)
 get_data(cart=True)
+
+
+my_varable = ""  # train case: my_variable_is_very_cool_and_holds_info
+myVar = ""  # camel case: weWouldWriteOurVariablesLikeThis
+str_myVar = ""
+int_myVar = 1
+float_myVar = 1.0
+bool_myVar = True
+str_my_var = ""
+
+
+str_myVar = "7"
+int_myVar = 7
+float_myVar = 7.0
